0ea9f2e6186522e1ebd869115eb1fbc920e782b7
agarwallakshya37 <37949887+agarwallakshya37@users.noreply.github.com>
2020-08-21 20:37:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from "react";
 import "./App.css";
 import Newpost from "./newpost";
+import Test from "./Test";
+import Map from "./MyMap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="header_logo">
-        <h1> LOGO </h1>
+    <Router>
+      <div className="App">
+        <div className="header_logo">
+          <h1> LOGO </h1>
+        </div>
+        <div className="body">
+          <Switch>
+            <Route path="/post">
+              <Newpost />
+            </Route>
+            <Route path="/test">
+              <Test />
+            </Route>
+            <Route path="/">
+              <Map />
+            </Route>
+          </Switch>
+        </div>
       </div>
-      <div >
-        <Newpost />
-      </div>
-    </div>
+    </Router>
   );
 }
 
